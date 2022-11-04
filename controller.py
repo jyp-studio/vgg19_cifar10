525c7c76ccf159f87d967b051a6f33f1eace1d96
jyp-studio <jackie1129008@gmail.com>
2022-11-04 20:55:26 +0800

    model

---
+++ controller.py
@@ -0,0 +1,160 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import QRunnable, QThreadPool, pyqtSlot, QObject, pyqtSignal
+from PyQt6.QtWidgets import QFileDialog
+from gui import Ui_MainWindow
+
+from PIL import Image
+from download_cifar10 import *
+import traceback, sys
+import cv2
+
+
+class WorkerSignals(QObject):
+    """
+    Defines the signals available from a running worker thread.
+
+    Supported signals are:
+
+    finished
+        No data
+
+    error
+        tuple (exctype, value, traceback.format_exc() )
+
+    result
+        object data returned from processing, anything
+
+    """
+
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+
+
+class Worker(QRunnable):
+    """
+    Worker thread
+
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    :param callback: The function callback to run on this worker thread. Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
+
+    """
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        """
+        Initialise the runner function with passed args, kwargs.
+        """
+
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)  # Return the result of the processing
+        finally:
+            self.signals.finished.emit()  # Done
+
+
+class Controller(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setup_control()
+
+        self.device = "mps"
+        self.data = Data()
+        self.filename = ""
+
+        # get some random training images
+        dataiter = iter(
+            torch.utils.data.DataLoader(
+                self.data.trainset, batch_size=9, shuffle=True, num_workers=0
+            )
+        )
+        self.images, self.labels = next(dataiter)
+
+        self.threadpool = QThreadPool()
+
+    def setup_control(self):
+        self.ui.btnLoad.clicked.connect(self.open_file)
+        self.ui.btn5_1.clicked.connect(self.show5_1)
+        self.ui.btn5_2.clicked.connect(self.show5_2)
+        self.ui.btn5_3.clicked.connect(self.show5_3)
+        self.ui.btn5_4.clicked.connect(self.show5_4)
+        self.ui.btn5_5.clicked.connect(self.show5_5)
+
+    def open_file(self):
+        self.filename, _ = QFileDialog.getOpenFileName(self, "Open file", "./")
+        self.display_img()
+
+    def display_img(self):
+        self.img = cv2.imread(self.filename)
+        # self.img = Image.open(self.filename)
+        # tensor = self.data.transform(self.img)
+        # self.img = self.data.unnormalize(tensor)
+        self.img = cv2.resize(self.img, (224, 224), interpolation=cv2.INTER_AREA)
+        height, width, channel = self.img.shape
+        bytesPerline = 3 * width
+        self.qimg = QtGui.QImage(
+            self.img, width, height, bytesPerline, QtGui.QImage.Format.Format_RGB888
+        ).rgbSwapped()
+        self.ui.labelImg.setPixmap(QtGui.QPixmap.fromImage(self.qimg))
+
+    def show5_1(self):
+        # show images
+        plt.figure()
+        for index, img in enumerate(self.images, start=1):
+            npimg = self.data.unnormalize(img)
+            plt.subplot(3, 3, index)
+            plt.title(f"{self.data.classes[labels[index - 1]]:5s}", fontsize=10)
+            plt.imshow(npimg)
+            plt.xticks([])
+            plt.yticks([])
+        plt.show()
+
+    def show5_2(self):
+        self.net = VGG19(10)
+        summary(self.net, (3, 32, 32), device="cpu")
+        self.net.to(device=self.device)
+
+    def show5_3(self):
+        plt.figure()
+        origin = self.data.unnormalize(self.images[0])
+        plt.subplot(2, 3, 2)
+        plt.title("origin")
+        plt.imshow(origin)
+        plt.xticks([])
+        plt.yticks([])
+        for i in range(4, 7):
+            tensor = self.data.augmentation(self.images[0])
+            trans = self.data.unnormalize(tensor)
+            plt.subplot(2, 3, i)
+            plt.title(f"trans{i-3}")
+            plt.imshow(trans)
+            plt.xticks([])
+            plt.yticks([])
+        plt.show()
+
+    def show5_4(self):
+        pass
+
+    def show5_5(self):
+        pass
--- download_cifar10.py
+++ download_cifar10.py
@@ -1,12 +1,14 @@
 import random
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 from torchsummary import summary
+
 
 # if gpu is avalible then use gpu
 device = "mps" if torch.backends.mps.is_available() else "cpu"
@@ -16,6 +18,7 @@ class Data:
     def __init__(self) -> None:
         self.transform = transforms.Compose(
             [
+                transforms.Resize(224),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ]
@@ -61,26 +64,18 @@ class Data:
 
         return npimg
 
-    def argumentation(self, tensor):
+    def augmentation(self, tensor):
         """
         Read torch tensor, do argumentation then return tensor
         """
 
         # define preprocessing step
-        degree = random.choices([0, 10, 20, 30])[0]
-        shearing_degree = random.choices([0, 10, 15, 20], weights=[60, 20, 10, 10])[0]
-        print(shearing_degree)
         preprocess = transforms.Compose(
             [
                 transforms.Resize(224),
-                transforms.ColorJitter(brightness=0.1, contrast=0.1),
+                transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
                 transforms.RandomHorizontalFlip(p=0.5),
-                torchvision.transforms.RandomAffine(
-                    degree,
-                    translate=None,
-                    scale=None,
-                    shear=((0, 0, 0, shearing_degree)),
-                ),
+                torchvision.transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
                 transforms.RandomGrayscale(p=0.3),
             ]
         )
@@ -121,7 +116,7 @@ class VGG19(nn.Module):
             ]
         )
         self.classifier = nn.Sequential(
-            nn.Linear(512, 4096),
+            nn.Linear(512 * 4 * 4, 4096),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
@@ -132,11 +127,11 @@ class VGG19(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        # out = out.view(out.size(0), -1)
-        logits = self.classifier(x.view(-1, 512))
-        probas = nn.functional.softmax(logits, dim=1)
+        x = x.view(x.size(0), -1)
+        # x = self.classifier(x.view(x.size(0), -1))
+        x = self.classifier(x)
 
-        return logits, probas
+        return x
 
     def _make_layers(self, cfg):
         layers = []
@@ -151,8 +146,44 @@ class VGG19(nn.Module):
                     nn.ReLU(inplace=True),
                 ]
                 in_channels = x
-        layers += [nn.AdaptiveAvgPool2d(7)]
+        layers += [nn.AdaptiveAvgPool2d(4)]
         return nn.Sequential(*layers)
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            3, 16, 3, 1, padding=1
+        )  # input is color image, hence 3 i/p channels. 16 filters, kernal size is tuned to 3 to avoid overfitting, stride is 1 , padding is 1 extract all edge features.
+        self.conv2 = nn.Conv2d(
+            16, 32, 3, 1, padding=1
+        )  # We double the feature maps for every conv layer as in pratice it is really good.
+        self.conv3 = nn.Conv2d(32, 64, 3, 1, padding=1)
+        self.fc1 = nn.Linear(
+            4 * 4 * 64, 500
+        )  # I/p image size is 32*32, after 3 MaxPooling layers it reduces to 4*4 and 64 because our last conv layer has 64 outputs. Output nodes is 500
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(
+            500, 10
+        )  # output nodes are 10 because our dataset have 10 different categories
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))  # Apply relu to each output of conv layer.
+        x = F.max_pool2d(x, 2, 2)  # Max pooling layer with kernal of 2 and stride of 2
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(
+            -1, 4 * 4 * 64
+        )  # flatten our images to 1D to input it to the fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(
+            x
+        )  # Applying dropout b/t layers which exchange highest parameters. This is a good practice
+        x = self.fc2(x)
+        return x
 
 
 if __name__ == "__main__":
@@ -183,32 +214,140 @@ if __name__ == "__main__":
     """
     Q5.2
     """
-    net = VGG19(10)
-    net.to(device=device)
-    # summary(net, (3, 32, 32))
+    model = VGG19(10)
+    summary(model, (3, 32, 32), device="cpu")
+    model.to(device=device)
     """
     Q5.3
     """
-    plt.figure()
-    origin = data.unnormalize(images[0])
-    plt.subplot(2, 3, 2)
-    plt.title("origin")
-    plt.imshow(origin)
-    plt.xticks([])
-    plt.yticks([])
-    for i in range(4, 7):
-        tensor = data.argumentation(images[0])
-        trans = data.unnormalize(tensor)
-        plt.subplot(2, 3, i)
-        plt.title(f"trans{i-3}")
-        plt.imshow(trans)
-        plt.xticks([])
-        plt.yticks([])
-    plt.show()
+    # plt.figure()
+    # origin = data.unnormalize(images[0])
+    # plt.subplot(2, 3, 2)
+    # plt.title("origin")
+    # plt.imshow(origin)
+    # plt.xticks([])
+    # plt.yticks([])
+    # for i in range(4, 7):
+    #     tensor = data.augmentation(images[0])
+    #     trans = data.unnormalize(tensor)
+    #     plt.subplot(2, 3, i)
+    #     plt.title(f"trans{i-3}")
+    #     plt.imshow(trans)
+    #     plt.xticks([])
+    #     plt.yticks([])
+    # plt.show()
 
     """
     Q5.4
     """
-    # define a Loss function and optimizer
+    # model = LeNet().to(device=device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    epochs = 2
+    running_loss_history = []
+    running_corrects_history = []
+    val_running_loss_history = []
+    val_running_corrects_history = []
+
+    for e in range(epochs):  # training our model, put input according to every batch.
+
+        running_loss = 0.0
+        running_corrects = 0.0
+        val_running_loss = 0.0
+        val_running_corrects = 0.0
+
+        for inputs, labels in data.trainloader:
+            model.train()
+            inputs = data.augmentation(inputs)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # every batch of 100 images are put as an input.
+            outputs = model(inputs)
+            # Calc loss after each batch i/p by comparing it to actual labels.
+            loss = criterion(outputs, labels)
+            # setting the initial gradient to 0
+            optimizer.zero_grad()
+            # backpropagating the loss
+            loss.backward()
+            # updating the weights and bias values for every single step.
+            optimizer.step()
+
+            # taking the highest value of prediction.
+            _, preds = torch.max(outputs, 1)
+            running_loss += loss.item()
+            # calculating te accuracy by taking the sum of all the correct predictions in a batch.
+            running_corrects += torch.sum(preds == labels.data)
+
+        else:
+            # we do not need gradient for validation.
+            with torch.no_grad():
+                for val_inputs, val_labels in data.testloader:
+                    model.eval()
+                    val_inputs = val_inputs.to(device)
+                    val_labels = val_labels.to(device)
+                    val_outputs = model(val_inputs)
+                    val_loss = criterion(val_outputs, val_labels)
+
+                    _, val_preds = torch.max(val_outputs, 1)
+                    val_running_loss += val_loss.item()
+                    val_running_corrects += torch.sum(val_preds == val_labels.data)
+
+            epoch_loss = running_loss / len(data.trainloader)  # loss per epoch
+            epoch_acc = running_corrects.float() / len(
+                data.trainloader
+            )  # accuracy per epoch
+            running_loss_history.append(epoch_loss)  # appending for displaying
+            print(epoch_acc.cpu())
+            running_corrects_history.append(epoch_acc.item())
+
+            val_epoch_loss = val_running_loss / len(data.testloader)
+            val_epoch_acc = val_running_corrects.float() / len(data.testloader)
+            val_running_loss_history.append(val_epoch_loss)
+            val_running_corrects_history.append(val_epoch_acc.item())
+            print("epoch:", (e + 1))
+            print(
+                "training loss: {:.4f}, acc {:.4f} ".format(
+                    epoch_loss, epoch_acc.item()
+                )
+            )
+            print(
+                "validation loss: {:.4f}, validation acc {:.4f} ".format(
+                    val_epoch_loss, val_epoch_acc.item()
+                )
+            )
+
+    PATH = "./cifar_net2.pth"
+    torch.save(model.state_dict(), PATH)
+
+    # loss
+    plt.style.use("ggplot")
+    plt.plot(running_loss_history, label="training loss")
+    plt.plot(val_running_loss_history, label="validation loss")
+    plt.legend()
+    plt.savefig("loss.png")
+    plt.show()
+
+    # accuracy
+    plt.style.use("ggplot")
+    plt.plot(running_corrects_history, label="training accuracy")
+    plt.plot(val_running_corrects_history, label="validation accuracy")
+    plt.legend()
+    plt.savefig("accuracy.png")
+    plt.show()
+
+    """
+    dataiter = iter(data.testloader)
+    images, labels = next(dataiter)
+
+    # print images
+    plt.figure()
+    for index, img in enumerate(images, start=1):
+        test = data.unnormalize(img)
+        plt.subplot(3, 3, index)
+        plt.title(f"{data.classes[labels[index - 1]]:5s}", fontsize=10)
+        plt.imshow(test)
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+    """
